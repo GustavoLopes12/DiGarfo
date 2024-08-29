6d82a843ed948bbd0cef4633a02a2d90d034c473
GustavoLopes12 <gustavolopess.dev@gmail.com>
2024-08-29 11:20:26 -0300

    backend comecei a fazer e coloquei dois model o receita e o usuario

---
+++ backend/digarfo/src/main/java/com/digarfo/digarfo/DigarfoApplication.java
@@ -0,0 +1,13 @@
+package com.digarfo.digarfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigarfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigarfoApplication.class, args);
+	}
+
+}
