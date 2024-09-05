6084382b77b9a1caaba3806858ea8faac7ea4df0
GustavoLopes12 <gustavolopess.dev@gmail.com>
2024-09-05 15:31:43 -0300

    editar perfil com barra agr

--- android/digarfo/app/src/main/java/com/example/digarfo/editarperfil.java
+++ android/digarfo/app/src/main/java/com/example/digarfo/editarperfil.java
@@ -1,6 +1,9 @@
 package com.example.digarfo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,27 @@ public class editarperfil extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    boolean usuariologado = true; //variavel usada em todas as funções
+    public void botaohome(View view){
+        Intent outraTela = new Intent(getApplicationContext(), home.class);
+        startActivity(outraTela);
+    }
+    public void irparainserir(View view){
+        if(usuariologado){
+            Intent outraTela = new Intent(getApplicationContext(), escreverreceita.class);
+            startActivity(outraTela);
+        }else{
+            Toast.makeText(this, "Faça login para escrever receitas :)", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void irparafavoritos(View view){
+        if (usuariologado){
+            Intent outraTela = new Intent(getApplicationContext(), favoritoslogado.class);
+            startActivity(outraTela);
+        }else{
+            Toast.makeText(this, "Você ainda não esta logado", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- backend/digarfo/src/main/java/com/digarfo/digarfo/Repository/UsuarioRepository.java
+++ backend/digarfo/src/main/java/com/digarfo/digarfo/Repository/UsuarioRepository.java
@@ -1,4 +1,6 @@
 package com.DiGarfo.DiGarfo.Repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -6,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import com.DiGarfo.DiGarfo.Model.Usuario;
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-	
-	/*@Query("SELECT u FROM usuario u WHERE u.nome_usuario LIKE %:nome_usuario%")
-	Iterable<Usuario> findByNome(String nome_usuario);*/
-	
+	/*
+	@Query("SELECT u FROM usuario u WHERE u.nome_usuario LIKE %:nome_usuario%")
+	Iterable<Usuario> findByNome(String nome_usuario);
+	*/
 }
