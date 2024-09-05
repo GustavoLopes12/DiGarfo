99a99acf2dacb20b941b5b93929ac076f0cf372e
GustavoLopes12 <gustavolopess.dev@gmail.com>
2024-09-05 16:38:53 -0300

    conexao_spring no android 1% quase nada feito mas começamos

--- android/digarfo/app/src/main/java/com/example/digarfo/conexao_spring/Conexao.java
+++ android/digarfo/app/src/main/java/com/example/digarfo/conexao_spring/Conexao.java
@@ -1,5 +1,13 @@
 package com.example.digarfo.conexao_spring;
 
 public class Conexao {
-
+    interface RequestUsuario{
+        //endpoints
+    }
+    interface RequestAdm{
+        //endpoints
+    }
+    String urlBase = "http://localhost:8080";
+    RequestUsuario requestUsuario;
+    RequestAdm requestAdm;
 }
