d465a06a9aee45566871a23810d1ba413d6d8a6d
anapolezi <anapolezi.dev@gmail.com>
2024-09-12 15:43:20 -0300

    fiz classes adm e admlist no model do android

---
+++ android/digarfo/app/src/main/java/com/example/digarfo/model/AdmList.java
@@ -0,0 +1,15 @@
+package com.example.digarfo.model;
+
+import java.util.List;
+
+public class AdmList {
+    List<Adm> lista_adm;
+
+    public List<Adm> getLista_adm() {
+        return lista_adm;
+    }
+
+    public void setLista_adm(List<Adm> lista_adm) {
+        this.lista_adm = lista_adm;
+    }
+}
