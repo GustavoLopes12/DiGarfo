ddeb4cd13b0fb894b19c566e9e7364023cb9a2a1
Nayan-Raven <nayan.raven@aluno.senai.br>
2024-09-12 15:40:34 -0300

    classes receita e receitalist no android

---
+++ android/digarfo/app/src/main/java/com/example/digarfo/model/ReceitaList.java
@@ -0,0 +1,16 @@
+package com.example.digarfo.model;
+
+import java.util.List;
+
+public class ReceitaList {
+
+    List<Receita> lista_receita;
+
+    public List<Receita> getLista_receita() {
+        return lista_receita;
+    }
+
+    public void setLista_receita(List<Receita> lista_receita) {
+        this.lista_receita = lista_receita;
+    }
+}
