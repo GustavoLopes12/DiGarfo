1e078ee677c22c0d8750d8a324334bc0cf2c4838
Nayan-Raven <nayan.raven@aluno.senai.br>
2024-09-17 08:18:34 -0300

    UsuarioRepository

--- backend/digarfo/src/main/java/com/digarfo/digarfo/Repository/UsuarioRepository.java
+++ backend/digarfo/src/main/java/com/digarfo/digarfo/Repository/UsuarioRepository.java
@@ -4,12 +4,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.digarfo.digarfo.Model.Usuario;
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, String>{
 	
 	/*@Query("SELECT u FROM usuario u WHERE u.nome_usuario = :nome_usuario")
-	Iterable<Usuario> findByNome_usuario(String nome_usuario);*/
+	Usuario findByNome_usuario(String nome_usuario);*/
+	
+	/*@Query("SELECT u FROM usuario u WHERE u.nome_usuario = :nome_usuario AND u.senha = :senha")
+	Usuario findByEmailAndSenha(@Param("email") String email, @Param("senha") String senha);*/
 	
 }
